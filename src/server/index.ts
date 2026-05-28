6c89129b11f1a6df2f681e041271a319d4877e14
penrodlol <christian@penrod.contact>
2026-05-28 19:43:16 -0400

    Remove future import

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,7 @@
 import tanstack from '@tanstack/react-start/server-entry';
-import { createQueueEventBody } from './queue';
 import scheduled from './scheduled';
 
 export default {
   fetch: tanstack.fetch as ExportedHandler['fetch'],
   scheduled,
-} satisfies ExportedHandler<Env, ReturnType<typeof createQueueEventBody>>;
+} satisfies ExportedHandler<Env>;
