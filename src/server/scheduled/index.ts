c55ff14e6b6f227908125b35a465201ca5d71530
penrodlol <christian@penrod.contact>
2026-05-28 21:22:31 -0400

    wrong cron pattern

--- src/server/scheduled/index.ts
+++ src/server/scheduled/index.ts
@@ -6,7 +6,7 @@ export const SCHEDULED_HANDLER_NO_HANDLER = 'No Handler for Scheduled';
 const scheduled: ExportedHandler<Env>['scheduled'] = async (event, env) => {
   try {
     switch (event.cron) {
-      case '* * * * *':
+      case '0 * * * *':
         return dailyScheduled(env);
       default:
         throw new Error(`${SCHEDULED_HANDLER_NO_HANDLER}: ${event.cron}`);
