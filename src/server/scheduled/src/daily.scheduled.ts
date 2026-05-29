220629419cd353dff205c801e72b58f3e2b8301d
penrodlol <christian@penrod.contact>
2026-05-28 21:28:02 -0400

    Add back DB

--- src/server/scheduled/src/daily.scheduled.ts
+++ src/server/scheduled/src/daily.scheduled.ts
@@ -1,12 +1,15 @@
 // export type DailyScheduledBody = Site & { items: Array<Pick<Parser.Item, 'title' | 'link' | 'pubDate'>> };
 
+import db from '@/db';
+
 export const DAILY_SCHEDULED_NO_SITES_ERROR = 'No Sites Found In Database';
 export const DAILY_SCHEDULED_PARSING_ERROR = 'Error Parsing RSS Feed';
 
 const dailyScheduled = async (_env: Env) => {
   console.log({ message: 'Daily Scheduled Task Started' });
-  // const sites = await db.select().from(site);
-  // if (!sites.length) throw new Error(DAILY_SCHEDULED_NO_SITES_ERROR);
+
+  const sites = await db.select().from(site);
+  if (!sites.length) throw new Error(DAILY_SCHEDULED_NO_SITES_ERROR);
 
   // const parser = new Parser();
   // const successful: Array<DailyScheduledBody> = [];
