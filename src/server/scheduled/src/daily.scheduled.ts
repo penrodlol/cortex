267efb0c8cb900bdb0b9ab60e7c4024aa40064b7
penrodlol <christian@penrod.contact>
2026-05-28 21:23:54 -0400

    Temp switching to every min

--- src/server/scheduled/src/daily.scheduled.ts
+++ src/server/scheduled/src/daily.scheduled.ts
@@ -1,12 +1,9 @@
-import { type Site } from '@/db';
-import Parser from 'rss-parser';
-
-export type DailyScheduledBody = Site & { items: Array<Pick<Parser.Item, 'title' | 'link' | 'pubDate'>> };
+// export type DailyScheduledBody = Site & { items: Array<Pick<Parser.Item, 'title' | 'link' | 'pubDate'>> };
 
 export const DAILY_SCHEDULED_NO_SITES_ERROR = 'No Sites Found In Database';
 export const DAILY_SCHEDULED_PARSING_ERROR = 'Error Parsing RSS Feed';
 
-const dailyScheduled = async (env: Env) => {
+const dailyScheduled = async (_env: Env) => {
   console.log({ message: 'Daily Scheduled Task Started' });
   // const sites = await db.select().from(site);
   // if (!sites.length) throw new Error(DAILY_SCHEDULED_NO_SITES_ERROR);
