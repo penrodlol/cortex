5575eb3664ec48491cea5092ae8c075d2b4be95d
penrodlol <christian@penrod.contact>
2026-07-05 18:04:33 -0400

    Begin inside this edition section entries

---
+++ src/components/accordion.tsx
@@ -0,0 +1,51 @@
+import { getComponentVariants } from '@/libs/variants';
+import * as AriaDisclosure from 'react-aria-components/Disclosure';
+import * as AriaDisclosureGroup from 'react-aria-components/DisclosureGroup';
+import { tv, type ClassValue, type VariantProps } from 'tailwind-variants';
+
+export type AccordionRootProps = React.ComponentProps<typeof AriaDisclosureGroup.DisclosureGroup>;
+export type AccordionItemProps = React.ComponentProps<typeof AriaDisclosure.Disclosure>;
+export type AccordionItemHeaderProps = Omit<React.ComponentProps<typeof AriaDisclosure.Button>, 'className'> &
+  AccordionItemHeaderVariants & { className?: ClassValue };
+export type AccordionItemPanelProps = Omit<React.ComponentProps<typeof AriaDisclosure.DisclosurePanel>, 'className'> &
+  AccordionItemPanelVariants & { className?: ClassValue };
+
+export type AccordionItemHeaderVariants = VariantProps<typeof accordionItemHeaderVariants>;
+export type AccordionItemPanelVariants = VariantProps<typeof accordionItemPanelVariants>;
+
+export const accordionItemHeaderVariants = tv({
+  extend: getComponentVariants({ slot: 'button', hover: true }).variants,
+  slots: { base: '', button: 'w-full py-3 text-left' },
+  defaultVariants: { variant: 'gray-ghost' },
+  compoundVariants: [
+    { variant: ['gray-ghost', 'gray-accent-ghost', 'gray-warn-ghost', 'gray-danger-ghost'], className: { button: 'text-gray-12' } },
+    { variant: 'accent-ghost', className: { button: 'text-accent-11' } },
+    { variant: 'warn-ghost', className: { button: 'text-warn-11' } },
+    { variant: 'danger-ghost', className: { button: 'text-danger-11' } },
+  ],
+});
+
+export const accordionItemPanelVariants = tv({
+  slots: { base: 'h-(--disclosure-panel-height) overflow-clip motion-safe:transition-[height]', content: 'pt-4 pb-8' },
+});
+
+export const Root = AriaDisclosureGroup.DisclosureGroup;
+export const Item = AriaDisclosure.Disclosure;
+
+export function ItemHeader({ className, ...props }: AccordionItemHeaderProps) {
+  const slots = accordionItemHeaderVariants();
+  return (
+    <AriaDisclosure.Heading className={slots.base({ className })}>
+      <AriaDisclosure.Button slot="trigger" className={slots.button()} {...props} />
+    </AriaDisclosure.Heading>
+  );
+}
+
+export function ItemPanel({ children, className, ...props }: AccordionItemPanelProps) {
+  const slots = accordionItemPanelVariants();
+  return (
+    <AriaDisclosure.DisclosurePanel className={slots.base({ className })} {...props}>
+      <div className={slots.content()}>{children}</div>
+    </AriaDisclosure.DisclosurePanel>
+  );
+}
--- src/routes/-_section-inside-this-edition.tsx
+++ src/routes/-_section-inside-this-edition.tsx
@@ -1,16 +1,55 @@
-import { Text } from '@/components/typography';
+import * as Accordion from '@/components/accordion';
+import { Avatar } from '@/components/avatar';
+import Link from '@/components/link';
+import { DateTime, Text } from '@/components/typography';
 import type { GetFeedResponse } from '@/server/function/feed';
 
-export type SectionInsideThisEditionProps = { entries: GetFeedResponse['entries'] };
+export type SectionInsideThisEditionProps = Pick<GetFeedResponse, 'entries'>;
 
 export default function SectionInsideThisEdition({ entries }: SectionInsideThisEditionProps) {
   return (
-    <section className="flex flex-col gap-8">
-      <div className="border-gray-6 flex items-center justify-center border-y-3 border-double py-4">
+    <section className="flex flex-col gap-12">
+      <div className="border-gray-6 flex items-center justify-center border-y-4 border-double py-5">
         <Text as="h2" font="serif" size="1">
           INSIDE THIS EDITION
         </Text>
       </div>
+      <Accordion.Root>
+        {entries.map((entry) => (
+          <Accordion.Item key={entry.url}>
+            <Accordion.ItemHeader className="-mx-4 not-lg:*:py-6">
+              <div className="px-4">
+                <div className="flex items-center justify-between gap-4">
+                  <div className="flex gap-2">
+                    <Text font="serif" variant="gray-soft" size="1">
+                      {entry.thumbnailUrl ? 'VIDEO' : 'ARTICLE'}
+                    </Text>
+                    <Text variant="gray-soft" size="1">
+                      //
+                    </Text>
+                    <DateTime value={entry.pubDate} font="serif" variant="gray-soft" size="1" />
+                  </div>
+                  <Link font="serif" variant="gray-soft" size="1" href={entry.sourceUrl} className="flex items-center gap-2">
+                    {entry.source}
+                    <Avatar size="1" src={entry.sourceLogoUrl} alt={entry.source.slice(0, 1)} />
+                  </Link>
+                </div>
+                <Text className="max-w-none">{entry.title}</Text>
+              </div>
+            </Accordion.ItemHeader>
+            <Accordion.ItemPanel>
+              <div className="flex flex-col gap-4">
+                <Text format="balance" variant="gray-soft" className="leading-6">
+                  {entry.summary}
+                </Text>
+                <Link font="serif" variant="gray-soft" href={entry.url}>
+                  {entry.thumbnailUrl ? 'Watch Video' : 'Read Article'}
+                </Link>
+              </div>
+            </Accordion.ItemPanel>
+          </Accordion.Item>
+        ))}
+      </Accordion.Root>
     </section>
   );
 }
