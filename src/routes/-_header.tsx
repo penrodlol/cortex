e27f4717d167ea31787ba3c152312061b403e761
penrodlol <christian@penrod.contact>
2026-07-31 17:12:33 -0400

    font size change

--- src/routes/-_header.tsx
+++ src/routes/-_header.tsx
@@ -3,12 +3,12 @@ import Search from './-_search';
 
 export default function Header() {
   return (
-    <header className="border-gray-6 flex items-center justify-between gap-4 border-b-6 border-double py-4 not-lg:flex-col not-lg:pb-6">
+    <header className="border-gray-6 flex justify-between gap-4 border-b-6 border-double py-4 not-lg:flex-col not-lg:pb-6 lg:items-center">
       <div>
-        <Text italic font="serif" size="5" weight="9" className="tracking-tighter not-lg:text-4xl">
+        <Text italic font="serif" size="5" weight="9" className="tracking-tighter not-lg:text-3xl">
           Cortex Digital Archive
         </Text>
-        <Text variant="gray-soft" size="2" className="not-lg:text-xl">
+        <Text variant="gray-soft" size="2" className="not-lg:text-lg">
           A Developer Artifact Registry
         </Text>
       </div>
