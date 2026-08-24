9d3412b23ca05862bee8b9777abe86de072f7a73
penrodlol <christian@penrod.contact>
2026-08-23 20:15:43 -0400

    Remove stats for tweets

---
+++ drizzle/0019_flat_ares.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `x_post` DROP COLUMN `likes`;--> statement-breakpoint
+ALTER TABLE `x_post` DROP COLUMN `reposts`;--> statement-breakpoint
+ALTER TABLE `x_post` DROP COLUMN `replies`;--> statement-breakpoint
+ALTER TABLE `x_post` DROP COLUMN `impressions`;
